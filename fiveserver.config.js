00229f108a912a15029f67cfd277b2df0c17211a
jorenham <jhammudoglu@gmail.com>
2024-01-13 02:08:32 +0100

    fiveserver setup

---
+++ fiveserver.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    root: 'lmo_web/',
+    host: '0.0.0.0',
+    open: 'index.html',
+    // uses a self-signed certificate
+    https: true,
+
+    // enable highlight feature
+    highlight: true,
+    // enable instant update
+    injectBody: true,
+    // enable remoteLogs
+    remoteLogs: true,
+    // disable injecting css
+    injectCss: false,
+    // enable auto-navigation
+    navigate: true,
+};
