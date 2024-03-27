fe9c636b74caa022dc4772cedfb5620e1faa9a64
jorenham <jhammudoglu@gmail.com>
2024-03-27 01:34:16 +0100

    fix ruff & pyright errors in `lmo_web.web_storage`

--- lmo_web/web_storage.py
+++ lmo_web/web_storage.py
@@ -7,14 +7,14 @@ __all__ = ('Storage', 'local_storage', 'session_storage')
 
 import json
 from collections.abc import Iterator, Mapping, MutableMapping, Sequence
-from typing import Final, TypeAlias, TypeVar
+from typing import Any, Final, Protocol, override, runtime_checkable
 
 from pyscript import window
 
 # https://github.com/python/typing/issues/182#issuecomment-1320974824
-JSON: TypeAlias = (
-    Mapping[str, 'JSON']
-    | Sequence['JSON']
+type _JSON = (
+    Mapping[str, '_JSON']
+    | Sequence['_JSON']
     | str
     | int
     | float
@@ -22,28 +22,49 @@ JSON: TypeAlias = (
     | None
 )
 
-V = TypeVar('V', bound=JSON)
+
+@runtime_checkable
+class JsStorage(Protocol):
+    # https://html.spec.whatwg.org/multipage/webstorage.html#storage-2
+    @property
+    def length(self) -> int: ...
+
+    def key(self, index: int, /) -> str | None: ...
+    def getItem(self, key: str, /) -> str | None: ...  # noqa: N802
+    def setItem(self, key: str, value: str, /) -> None: ...  # noqa: N802
+    def removeItem(self, key: str, /) -> None: ...  # noqa: N802
+    def clear(self, /) -> None: ...
 
 
-class Storage(MutableMapping[str, V]):
+class Storage[V: _JSON](MutableMapping[str, V]):
     __slots__ = ('_storage', )
 
-    def __init__(self, storage):
+    _storage: Final[JsStorage]
+
+    def __init__(self, storage: JsStorage, /):
         self._storage = storage
 
     __hash__ = None
 
+    @override
     def __len__(self) -> int:
         return self._storage.length
 
+    @override
     def __iter__(self) -> Iterator[str]:
         storage = self._storage
         for i in range(storage.length):
-            yield storage.key(i)
+            name = storage.key(i)
+            assert name is not None
+            yield name
 
+    @override
     def __contains__(self, key: object) -> bool:
+        if not isinstance(key, str):
+            return False
         return self._storage.getItem(key) is not None
 
+    @override
     def __getitem__(self, key: str) -> V:
         value = self._storage.getItem(key)
         if value is None:
@@ -51,15 +72,18 @@ class Storage(MutableMapping[str, V]):
 
         return json.loads(value)
 
+    @override
     def __setitem__(self, key: str, value: V) -> None:
         self._storage.setItem(key, json.dumps(value))
 
+    @override
     def __delitem__(self, key: str) -> None:
         self._storage.removeItem(key)
 
+    @override
     def clear(self) -> None:
         self._storage.clear()
 
 
-local_storage: Final[Storage] = Storage(window.localStorage)  # type: ignore
-session_storage: Final[Storage] = Storage(window.sessionStorage) # type: ignore
+local_storage: Final[Storage[Any]] = Storage(window.localStorage)  # type: ignore  # noqa: PGH003
+session_storage: Final[Storage[Any]] = Storage(window.sessionStorage)  # type: ignore  # noqa: PGH003
